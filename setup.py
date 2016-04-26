0e39fae17186e4e8fd231187002f556d8f375095
jeffgodwyll <jeffgodwyll@gmail.com>
2016-04-26 02:17:43 +0000

    Adds missing author meta-data

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     name='flagen',
     version='0.1',
     url='https://github.com/jeffgodwyll/flagen',
+    author='Jeffrey Godwyll',
     author_email='hi@jeffgodwyll.com',
     description='A flask static site generator that uses markdown and jinja2 '
     'templates.',
